9662cea6d434f6b777492a1e03ddbbd6f9da40f9
Darina-Shvydiuk <104676845+Darina-Shvydiuk@users.noreply.github.com>
2022-12-09 12:13:19 +0100

    finish lesson 0 and hw done

---
+++ initial/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <MyComponent/>
+  <HwCounter/>
+</template>
+
+<script>
+import MyComponent from './components/lesson0/MyComponent.vue'
+import HwCounter from './components/lesson0/HwCounter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent,
+    HwCounter
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ initial/src/components/lesson0/HwCounter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="root">
+    <p>
+      Выберите кол-во продуктов:
+    </p>
+    <button
+      type="button"
+      @click="decreaseCount"
+    >
+      -
+    </button>
+      <input
+        type="number"
+        :value="count"
+        :min="minCount"
+        :max="maxCount"
+      >
+    <button
+      type="button"
+      @click="increaseCount"
+    >
+      +
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data () {
+    return {
+      count: 1,
+      maxCount: 10,
+      minCount: 1
+    }
+  },
+  methods: {
+    increaseCount () {
+      if (this.count < this.maxCount) {
+        this.count++
+      }
+    },
+    decreaseCount () {
+      if (this.count > this.minCount) {
+        this.count--
+      }
+    }
+  }
+}
+</script>
